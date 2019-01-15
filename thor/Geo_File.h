225dd5004df62407622eddd6e7eeb4ab5ae7326e
Joe Best-Rotheray <joebestrotheray@gmail.com>
2019-01-15 13:32:48 +0000

    removed some dead geo file code

--- thor/Geo_File.h
+++ thor/Geo_File.h
@@ -4,6 +4,4 @@
 
 
 
-void geo_file_check(File_Handle file, struct Linear_Allocator* allocator);
-
 void geo_file_read(File_Handle file, const char** model_names, struct Model* out_models, int32 model_count, Linear_Allocator* model_allocator, Linear_Allocator* temp_allocator);
