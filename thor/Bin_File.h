d503fe23f9a2f1fe8ec4a380b4e8c50e1bf57a7f
Joe Best-Rotheray <joebestrotheray@gmail.com>
2019-01-18 13:02:40 +0000

    removed a bunch of stuff that was just for testing geobins

--- thor/Bin_File.h
+++ thor/Bin_File.h
@@ -7,16 +7,9 @@
 
 constexpr uint8 c_bin_file_sig[8] = { 0x43, 0x72, 0x79, 0x70, 0x74, 0x69, 0x63, 0x53 };
 constexpr uint32 c_bin_geobin_type_id = 0x3e7f1a90;
-constexpr uint32 c_bin_bounds_type_id = 0x9606818a;
-constexpr uint32 c_bin_origins_type_id = 0x6d177c17;
 constexpr uint32 c_bin_defnames_type_id = 0x0c027625;
 
 
-
-/**
- * check a bin file can be successfully parsed
- */
-void bin_file_check(File_Handle file);
 
 void geobin_file_read(
 	File_Handle file, 
